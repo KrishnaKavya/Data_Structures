7966dee7d00b9530c35d57f5efa0218c040e7c45
Maringanty Krishna Kavya <m.krishnakavya@gmail.com>
2016-03-22 00:50:54 -0500

    Update QueueImplementation.java

--- QueueImplementation.java
+++ QueueImplementation.java
@@ -1,41 +1,74 @@
 public class QueueImplementation {
 
 	public int queue[];
-	public int last=0,first=0, size;
+	public int rear = -1, front = -1, size;
 
 	QueueImplementation(int size) {
-		queue = new int[size];
+		this.queue = new int[size];
 		this.size = size;
 	}
 
 	public void enqueue(int element) {
 		if (isFull()) {
 			System.out.println("queue is full cannot perform insertion operation.");
-		} else {
-			queue[last++] = element;
+		} else if(rear==-1){
+			front=0;
+			rear=0;
+			queue[rear]=element;
+		}else if(rear+1<size){
+			queue[++rear]=element;
 		}
 	}
 
 	public void dequeue() {
 		if (isEmpty()) {
 			System.out.println("queue is empty cannot perform deletion operation.");
-		} else {
-			System.out.println(queue[first]);
-			first++;
+		} else{
+			front++;
+			if(front>rear){
+				reinitialize();
+			}
+			
 		}
 
 	}
 
+	private void reinitialize() {
+		System.out.println("All the elements in the queue are processed. re initialising the queue");
+		front=-1;
+		rear=-1;
+		
+	}
+
 	public boolean isEmpty() {
+		if (front == -1) {
+			return true;
+		}
 		return false;
 	}
 
 	public boolean isFull() {
+		if (rear == size - 1 ) {
+			return true;
+		}
 		return false;
+
+	}
+	public void display(){
+		if(isEmpty()){
+			System.out.println("Queue is empty");
+		}else{
+		for(int i= front;i<=rear;i++){
+			System.out.print(queue[i]+" ");
+		}
+	}
 	}
 
 	public static void main(String[] args) {
-
+		
+		int size=10;
+		QueueImplementation obj=new QueueImplementation(size);
+		
 	}
 
 }
