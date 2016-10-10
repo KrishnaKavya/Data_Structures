254ab211e89c7667fe923375723b010e6ea6d0a4
KrishnaKavya <m.krishnakavya@gmail.com>
2016-10-10 12:06:57 -0500

    dfs complete

--- src/DepthFirstSearch.java
+++ src/DepthFirstSearch.java
@@ -11,9 +11,27 @@ import java.util.Set;
 /**
  * @author M Krishna Kavya
  * 
+ *         Depth-first search (DFS) is an algorithm for traversing or searching
+ *         tree or graph data structures. One starts at the root (selecting some
+ *         arbitrary node as the root in the case of a graph) and explores as
+ *         far as possible along each branch before backtracking.
+ * 
+ *         classes: Graph,Vertex,Edge.
+ * 
  */
 public class DepthFirstSearch {
 
+	/**
+	 * The method depth First Search searches or parse through the start node of
+	 * the graph. The Data structures used are stack and a Hashset to keep track
+	 * of the vertices visited. for all the vertices in the graph, we check if
+	 * the vertex is already processed (the vertices already visited are
+	 * considered as processed). if true, the dfs is continued, else the dfsUtil
+	 * is called to process the vertex.
+	 * 
+	 * 
+	 * @param graphObject
+	 */
 	public void depthFirstSearch(Graph graphObject) {
 		Deque<Vertex> stack = new ArrayDeque<Vertex>();
 		Set<Vertex> visited = new HashSet<Vertex>();
@@ -28,6 +46,25 @@ public class DepthFirstSearch {
 		}
 	}
 
+	/**
+	 * This method processes the dfs of the vertex in in the paramenter. The
+	 * stack has the current processing vertices and visited has the vertices
+	 * already processed.
+	 * 
+	 * we push the first vertex(start) into the stack.
+	 * 
+	 * while stack is not empty, we pop the element from the stack. mark it as
+	 * visited and display in the output list.
+	 * 
+	 * Fetch all the adjacent vertices of the current vertex for all the
+	 * adjacent vertices. check if the element is present in the visited elist
+	 * if yes continue with the loop else process that vertex by recursively
+	 * calling dfsUtil.
+	 * 
+	 * @param current
+	 * @param stack
+	 * @param visited
+	 */
 	public void dfsUtil(Vertex current, Deque<Vertex> stack, Set<Vertex> visited) {
 		current.seen = true;
 		visited.add(current);
