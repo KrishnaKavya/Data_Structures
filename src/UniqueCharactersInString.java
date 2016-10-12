bf2f2aa6fee00b66c930217ea01747d2b6086955
KrishnaKavya <m.krishnakavya@gmail.com>
2016-10-12 18:46:28 -0500

    UniqueCharactersDoc

--- src/UniqueCharactersInString.java
+++ src/UniqueCharactersInString.java
@@ -10,38 +10,57 @@ import java.util.Scanner;
  */
 public class UniqueCharactersInString {
 	/**
-	 * Approach 1: 
+	 * Approach 1
+	 * 
+	 * The method validates returns true if the string is unique else returns
+	 * false.
+	 * 
+	 * Implementation:
+	 * 
+	 * The A list of type Character is initialized. The input string to
+	 * converted to a character array. For each character in a string, we check
+	 * is the character is already in the Array List. If yes, A character is
+	 * already in the list. That is it is repeated. return false. else, we add
+	 * the character to the List. The above process is done for all the
+	 * characters in the string.
+	 * 
+	 * If the loop is not broken that implies all the characters are processed
+	 * and there was no character being repeated. Hence, return false at the end
+	 * of function call.
+	 * 
 	 * @param string
 	 * @return
 	 */
-	public boolean validate(String string){
-		if(string==null){
+	public boolean validate(String string) {
+		if (string == null) {
 			return false;
 		}
-		List <Character> list=new ArrayList<Character>();
-		char[] charArray=string.toCharArray();
-		for(int i=0;i<charArray.length;i++){
-			if(list.contains(charArray[i])){
+		List<Character> list = new ArrayList<Character>();
+		char[] charArray = string.toCharArray();
+		for (int i = 0; i < charArray.length; i++) {
+			if (list.contains(charArray[i])) {
 				return false;
-			}else{
+			} else {
 				list.add(charArray[i]);
 			}
 		}
-		
-	return true;
+
+		return true;
 	}
 
 	public static void main(String[] args) {
-		UniqueCharactersInString obj=new UniqueCharactersInString();
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Please give an input string to validate if the string has unique characters");
-		boolean result=obj.validate(sc.next());
-		if(result){
+		UniqueCharactersInString obj = new UniqueCharactersInString();
+		Scanner sc = new Scanner(System.in);
+		System.out
+				.println("Please give an input string to validate if the string has unique characters");
+		boolean result = obj.validate(sc.next());
+		if (result) {
 			System.out.println("The String has unique characters");
-		}else{
-			System.out.println("The input is empty/The String does not have unique characters");
+		} else {
+			System.out
+					.println("The input is empty/The String does not have unique characters");
 		}
-		
+
 		sc.close();
 	}
 
