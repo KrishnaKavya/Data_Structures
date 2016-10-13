9213e45ed13bc815b4a7a7ccc7bb097a136228ae
KrishnaKavya <m.krishnakavya@gmail.com>
2016-10-13 10:56:46 -0500

    reverse0.1

--- src/ReverseOfString.java
+++ src/ReverseOfString.java
@@ -1,13 +1,23 @@
+import java.util.Scanner;
 
 public class ReverseOfString {
 
-	
-	
+	public void reverse(String inputString) {
+
+		char[] charArray = inputString.toCharArray();
+		StringBuilder sb = new StringBuilder();
+		for (int i = charArray.length - 1; i >= 0; i--) {
+			sb.append(charArray[i]);
+		}
+		System.out.println(sb.toString());
+	}
 
 	public static void main(String[] args) {
-		ReverseOfString obj=new ReverseOfString();
-		
-		
+		ReverseOfString obj = new ReverseOfString();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please enter the string to reverse.");
+		String inputString = sc.next();
+		obj.reverse(inputString);
 
 	}
 
