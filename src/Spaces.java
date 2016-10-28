2518a4bad223c1e93da45a074cab1259cb6f7597
KrishnaKavya <m.krishnakavya@gmail.com>
2016-10-28 11:50:10 -0500

    Spaces Problem

--- src/Spaces.java
+++ src/Spaces.java
@@ -1,22 +1,28 @@
-
+/**
+ * 
+ * @author M Krishna Kavya
+ * 
+ * The Spaces class replaces the spaces in a sentence with %. 
+ */
 public class Spaces {
-	
-	public char[] spaces(String input){
-		char[] charArray=input.toCharArray();
-		for(int i=0;i<charArray.length;i++){
-			if(charArray[i]==' '){
-				charArray[i]='%';
-			}else{
+
+	public char[] spaces(String input) {
+		char[] charArray = input.toCharArray();
+		for (int i = 0; i < charArray.length; i++) {
+			if (charArray[i] == ' ') {
+				charArray[i] = '%';
+			} else {
 				continue;
 			}
 		}
-		
+
 		return charArray;
-		
+
 	}
+
 	public static void main(String[] args) {
-		Spaces obj=new Spaces();
-		char[] result=obj.spaces("The Scentance has spaces");
+		Spaces obj = new Spaces();
+		char[] result = obj.spaces("The Scentance has spaces");
 		System.out.println(new String(result));
 
 	}
