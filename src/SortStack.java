10c3e55eb31186c5095b22f9af186ec395f1374a
KrishnaKavya <m.krishnakavya@gmail.com>
2016-11-02 11:57:50 -0500

    doc

--- src/SortStack.java
+++ src/SortStack.java
@@ -5,11 +5,19 @@ import java.util.Deque;
  * 
  * @author M Krishna Kavya
  * 
- *         The class implements a method two sort a stack using one buffer
- *         stack. This is done without using any other data structure. using the
- *         functions of stack. push, pop,peek.
+ *         The class implements a method two sort a stack in descending order
+ *         using one buffer stack. This is done without using any other data
+ *         structure. using the functions of stack. push, pop,peek.
  * 
- * 			 
+ *         Algorithm: The original stack(originalstack) has all the elements.
+ *         the second stack stores all the elements in a sorted order. 1. pop
+ *         the element from stack originalstack and save it in a temporary
+ *         variable current. while the second stack(sortStack) is not empty and
+ *         if the current is less than the top. we pop the element from stack
+ *         and push it into original stack. This process is repeated till
+ *         original stack is empty.
+ * 
+ * 
  *
  */
 public class SortStack {
@@ -17,12 +25,18 @@ public class SortStack {
 	Deque<Integer> stack = new ArrayDeque<Integer>();
 	Deque<Integer> sortStack = new ArrayDeque<Integer>();
 
+	/**
+	 * adding sample elements in a stack.
+	 */
 	public void add() {
 		for (int i = 0; i < 10; i++) {
 			stack.push(i);
 		}
 	}
 
+	/**
+	 * sorting a stack
+	 */
 	public void sort() {
 		while (!stack.isEmpty()) {
 			int current = stack.pop();
@@ -34,7 +48,11 @@ public class SortStack {
 
 		display(sortStack);
 	}
-
+	
+	/**
+	 * Display a stack
+	 * @param stack
+	 */
 	public void display(Deque<Integer> stack) {
 		while (!stack.isEmpty()) {
 			System.out.println(stack.pop());
