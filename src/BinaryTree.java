4706b51229e47a9a14654b65693b66bb7c133529
KrishnaKavya <m.krishnakavya@gmail.com>
2016-11-09 11:47:32 -0600

    BT

---
+++ src/BinaryTree.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ * @author M Krishna Kavya
+ * 
+ * 
+ */
+public class BinaryTree {
+	
+	public class Node{
+		
+		private int value;
+		private Node leftNode;
+		private Node rightNode;
+		private int height;
+		
+		public Node(int value, Node leftNode, Node rightNode, int height) {
+			super();
+			this.value = value;
+			this.leftNode = leftNode;
+			this.rightNode = rightNode;
+			this.height = height;
+		}
+	}
+		
+	public static void main(String[] args) {
+		
+
+	}
+
+}
