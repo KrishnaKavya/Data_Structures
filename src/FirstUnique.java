df525a6691bdbb2a8445580104b17f8cac8ed412
KrishnaKavya <m.krishnakavya@gmail.com>
2017-01-23 12:07:42 -0600

    First Unique

--- src/FirstUnique.java
+++ src/FirstUnique.java
@@ -7,31 +7,34 @@ import java.util.Map.Entry;
 
 public class FirstUnique {
 
-	public void firstUnique(String params) {
+	public String firstUnique(String params) {
 
 		char[] charArray = params.toCharArray();
 		Map<Character, Integer> map = new HashMap<Character, Integer>();
-		Deque<Character> queue=new ArrayDeque<Character>();
-		for(int i=0;i<charArray.length;i++){
-			if(!map.containsKey(charArray[i])){
+		Deque<Character> queue = new ArrayDeque<Character>();
+		for (int i = 0; i < charArray.length; i++) {
+			if (!map.containsKey(charArray[i])) {
 				map.put(charArray[i], 1);
-			}else{
-				map.put(charArray[i], (map.get(charArray[i])+1));
+				queue.add(charArray[i]);
+			} else {
+				map.put(charArray[i], (map.get(charArray[i]) + 1));
+				queue.add(charArray[i]);
 			}
 		}
-		
-		Iterator<Entry<Character, Integer>> itr=map.entrySet().iterator();
-		
-		while(itr.hasNext()){
-			Map.Entry<Character,Integer> entry = (Map.Entry<Character,Integer>) itr.next();
-			System.out.println("Key : " + entry.getKey() + " Value :" + entry.getValue());
+
+		while (!queue.isEmpty()) {
+			char current = queue.remove();
+			if (map.get(current) == 1) {
+				return "The first unique character is " + current;
+			}
 		}
-		
+		return "The String Does not have unique characters";
 	}
-	//System.out.println(i+" "+charArray[i]);
+
 	public static void main(String[] args) {
 		FirstUnique obj = new FirstUnique();
-		obj.firstUnique("kavyak");
+		String result = obj.firstUnique("kavyak");
+		System.out.println(result);
 	}
 
 }
