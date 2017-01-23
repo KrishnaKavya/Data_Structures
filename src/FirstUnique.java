1031637e47d4eb98b3b61d97310c90609ae49f11
KrishnaKavya <m.krishnakavya@gmail.com>
2017-01-22 18:55:56 -0600

    new

---
+++ src/FirstUnique.java
@@ -0,0 +1,12 @@
+
+public class FirstUnique {
+
+	public void firstUnique(String params){
+		
+	}
+	public static void main(String[] args) {
+		FirstUnique obj=new FirstUnique();
+		obj.firstUnique("kavyak");
+	}
+
+}
