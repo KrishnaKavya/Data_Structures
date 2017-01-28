4d089cc02d72e4a038edc3e145a15fa91569ca6b
KrishnaKavya <m.krishnakavya@gmail.com>
2017-01-27 22:02:03 -0600

    pattern2

--- src/Pattern.java
+++ src/Pattern.java
@@ -4,20 +4,51 @@
  *
  */
 public class Pattern {
-	
-	public void one(){
-		for(int i=1;i<6;i++){
-			for(int j=0;j<i;j++){
+	/**
+	 * Method to print pattern
+	 * 
+	 * * 
+	 * ** 
+	 * *** 
+	 * **** 
+	 * *****
+	 */
+	public void one() {
+		for (int i = 1; i < 6; i++) {
+			for (int j = 0; j < i; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
-			
+
 		}
 	}
-	
+	/**
+	 * Method prints the pattern
+	 * 
+	 *     *
+	 *    **
+	 *   ***
+	 *  ****
+	 * *****	
+	 */
+	public void two() {
+		int i = 1, j = 6;
+		for (int k = 1; k < j; k++) {
+			for (int c = 1; c < (j - i); c++) {
+				System.out.print(" ");
+			}
+			for (int c = 1; c <= i; c++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			i++;
+		}
 
-	public static void main(String args[]){
-		Pattern obj=new Pattern();
-		obj.one();
+	}
+
+	public static void main(String args[]) {
+		Pattern obj = new Pattern();
+		// obj.one();
+		obj.two();
 	}
 }
