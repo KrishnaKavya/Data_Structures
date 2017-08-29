a5d3e38ac686fe62fbec00467289d28ac2c6c836
KrishnaKavya <m.krishnakavya@gmail.com>
2017-08-29 11:17:26 -0500

    bubble sort Update

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,12 +1,12 @@
+import java.util.Arrays;
+
 /**
  * 
  * 
- * Bubble sort is an algorithm that sorts an array of elements in increasing order by comparing adjacent elements. 
+ * Bubble sort is an algorithm that sorts an array of elements in increasing
+ * order by comparing adjacent elements.
  * 
- * Analysis of Bubble Sort:
- * worst case: O(n2)
- * Average: O(n2)
- * Best: O(n)
+ * Analysis of Bubble Sort: worst case: O(n2) Average: O(n2) Best: O(n)
  * 
  * @author M Krishna Kavya
  *
@@ -14,29 +14,35 @@
 
 public class BubbleSort {
 	/**
-	 *  
-	 * 
-	 * we start comparision with the one element with its adjacent element. 
-	 * if swap the elements of the left element is greater than the right.
-	 * we now consider the right element and its adjacent element. 
-	 * This operation is performed to all the elements in the array. 
-	 * 
-	 * Once we reach the end of the array performing the swaps. The largest element is placed at the last index. 
-	 * The process of comparision from start till the end of an array is called as a pass. 
-	 * 
-	 * Depending on the input . after a certain number of pass the array will be sorted. 
 	 * 
 	 * 
-	 * Optimization: 
+	 * we start comparision with the one element with its adjacent element. if
+	 * the elements of the left element is greater than the right then swap. we now
+	 * consider the right element and its adjacent element. This operation is
+	 * performed to all the elements in the array.
 	 * 
-	 * 1. At the end of every pass the higher elements gets placed in order in the end.
-	 *    so, It is not required to perform comparision and swaps. Hence, we reduce the iterations of the inner loop size-1 to size-i-1.
-	 * 2. After certain number of pass operations. The array is already sorted. (Eg: there are 10 elements in the array the 
-	 *    outer loop runs for n times but the elements get sorted in 3rd iteration).When the array is already sorted
-	 *    there are no swaps being performed. Hence, we set a flag to check if swaps are performed. Once there are no swaps 
-	 *    we can break the loop and save the rest of iterations. 
+	 * Once we reach the end of the array performing the swaps. The largest
+	 * element is placed at the last index. The process of comparision from
+	 * start till the end of an array is called as a pass.
 	 * 
-	 * @param input- array of elements.
+	 * Depending on the input . after a certain number of pass the array will be
+	 * sorted.
+	 * 
+	 * 
+	 * Optimization:
+	 * 
+	 * 1. At the end of every pass the higher elements gets placed in order in
+	 * the end. so, It is not required to perform comparision and swaps. Hence,
+	 * we reduce the iterations of the inner loop size-1 to size-i-1. 2. After
+	 * certain number of pass operations. The array is already sorted. (Eg:
+	 * there are 10 elements in the array the outer loop runs for n times but
+	 * the elements get sorted in 3rd iteration).When the array is already
+	 * sorted there are no swaps being performed. Hence, we set a flag to check
+	 * if swaps are performed. Once there are no swaps we can break the loop and
+	 * save the rest of iterations.
+	 * 
+	 * @param input
+	 *            - array of elements.
 	 */
 	public static void bubbleSort(int input[]) {
 		int size = input.length;
@@ -54,16 +60,13 @@ public class BubbleSort {
 				break;
 			}
 		}
-
-		// Displaying the sorted array.
-		for (int i = 0; i < size; i++) {
-			System.out.println(input[i]);
-		}
 	}
 
 	public static void main(String[] args) {
 		int[] input = { 7, 1, 3, 5, 4 };
 		bubbleSort(input);
+		System.out.println(Arrays.toString(input));
+		
 	}
 
 }
