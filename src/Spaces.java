693544b3b80beccb222fc3b1e676674b6562c9c5
KrishnaKavya <m.krishnakavya@gmail.com>
2017-09-10 18:19:44 -0500

    Spaces

--- src/Spaces.java
+++ src/Spaces.java
@@ -2,21 +2,39 @@
  * 
  * @author M Krishna Kavya
  * 
- * The Spaces class replaces the spaces in a sentence with %. 
+ *         The Spaces class replaces the spaces in a sentence with %.
  */
 public class Spaces {
 
 	public char[] spaces(String input) {
-		char[] charArray = input.toCharArray();
-		for (int i = 0; i < charArray.length; i++) {
-			if (charArray[i] == ' ') {
-				charArray[i] = '%';
+
+		int spaceCount = 0;
+		int length = input.length();
+		char[] str = input.toCharArray();
+		
+		for (int i = 0; i < length; i++) {
+			if (input.charAt(i) == ' ') {
+				spaceCount++;
+			}
+		}
+		//new String length.
+		int newLength = length + (spaceCount * 2);
+		
+		char[] newString = new char[(newLength)];
+
+		for (int i = length - 1; i >= 0; i--) {
+			if (str[i] == ' ') {
+				newString[newLength - 1] = '0';
+				newString[newLength - 2] = '2';
+				newString[newLength - 3] = '%';
+				newLength = newLength - 3;
 			} else {
-				continue;
+				newString[newLength - 1] = str[i];
+				newLength = newLength - 1;
 			}
 		}
 
-		return charArray;
+		return newString;
 
 	}
 
